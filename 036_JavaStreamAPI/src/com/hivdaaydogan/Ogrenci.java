cf406bc179e7bc2303e45c828c27628b8e55bc26
unknown <hivdaydgn@gmail.com>
2023-03-07 00:09:40 +0300

    Stream API

---
+++ 036_JavaStreamAPI/src/com/hivdaaydogan/Ogrenci.java
@@ -0,0 +1,47 @@
+package com.hivdaaydogan;
+
+import java.util.Random;
+
+public class Ogrenci {
+
+	private String ad;
+	private int id;
+	
+	
+	public Ogrenci(String ad, int id) {
+		super();
+		this.ad = ad;
+		this.id = id;
+	}
+
+
+	public String getAd() {
+		return ad;
+	}
+
+
+	public void setAd(String ad) {
+		this.ad = ad;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Ogrenci [ad=" + ad + ", id=" + id + "]";
+	}
+	
+	public void printSinavNotu() {
+		Random r = new Random();
+		System.out.println("Öğrenci : " + this.ad + " sınav notu: " + r.nextInt(100));
+	}
+}
---
+++ 038_AnonymousInnerClasses_Interface/src/com/hivdaaydogan/IUcabilir.java
@@ -0,0 +1,7 @@
+package com.hivdaaydogan;
+
+public interface IUcabilir {
+
+	void uc();
+	
+}
